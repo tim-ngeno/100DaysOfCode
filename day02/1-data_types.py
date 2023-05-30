443124a3bcf936aef629963d326846bd52106b04
Tim Ngeno <h34r54y5@gmail.com>
2023-05-30 20:04:21 +0300

    use different data types in python

---
+++ day02/1-data_types.py
@@ -0,0 +1,23 @@
+# Data Types
+# String - A string of characters e.g
+
+"Hello"
+
+# Pulling a character from a script is called subscripting
+print("Hello"[3]) # should print the value at index 3, starting from 0
+
+# Integer - Whole numbers (negative and positive)
+print(123 + 456)
+
+# Float - decimal numbers
+3.14159
+
+# Boolean
+True
+False
+
+
+name = len(input("What is your name?\n"))
+length = str(name) # changing type of len to string aka type casting
+
+print("Your name has " + length + " characters")

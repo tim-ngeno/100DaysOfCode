6ea2eab15f8281c20948b09fb5bd65fb17813da3
Tim Ngeno <h34r54y5@gmail.com>
2023-05-30 20:10:16 +0300

    use arithmetic operators to perform calculations

---
+++ day02/3-operations.py
@@ -0,0 +1,41 @@
+from decimal import DivisionByZero
+
+3 + 3
+4 - 2
+2 * 4
+6 / 3
+2 ** 3 # (to the power of ..)
+
+
+# #######   THE ORDER OF OPERATIONS #######
+
+# PEMDAS
+# Parentheses - ()
+# Exponents - **
+# Multiplication - *
+# Division - /
+# Addition - +
+# Subtraction - -
+
+
+# in python:
+
+# ()
+# **
+# * or /
+# + or -
+
+# In Python programming, division and multiplication
+# are equally important, and the same goes for addition and subtraction
+
+# When there are multiple operations to be performed on a single line
+# of code, precedence will be given to the operation at the farthest left
+
+#  IMPORTANT => CALCULATION GOES FROM LEFT TO RIGHT
+
+
+# print(3 * 3 + 3 / 3 - 3)
+
+
+# Change the code to get a '3'
+print(3 + 3 - 3 / 3 * 3)

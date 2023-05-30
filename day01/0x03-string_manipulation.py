e0b5bd539a51065e98de7416b92ee42842ef34b7
Tim Ngeno <h34r54y5@gmail.com>
2023-05-30 18:35:08 +0300

    manipulate strings inside the print function

---
+++ day01/0x03-string_manipulation.py
@@ -0,0 +1,7 @@
+# using newline character to print multiple lines using print function 
+
+print("Hello, World!\nThis is a new line in python\n")
+
+# Concatenation using '+' sign
+# combining two+ strings into one
+print("Hello, " + "Tim")

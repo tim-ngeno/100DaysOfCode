01628435d5f72a003b4900f853b7aacfacc3e8ea
Tim Ngeno <h34r54y5@gmail.com>
2023-05-30 18:52:49 +0300

    use the input() function to get user input

---
+++ day01/0x05-input_function.py
@@ -0,0 +1,7 @@
+# taking input from a user
+# input function always returns data type string
+
+# input("What is your name? ")
+
+# input() gets the user input and print adds "Hello" before the name
+print("Hello, " + input("What is your name? "))

662f9fd88339333e0fedc8c2cc986fb1aed985e8
Tim Ngeno <h34r54y5@gmail.com>
2023-05-30 18:34:39 +0300

    print output on multiple lines using print()

---
+++ day01/0x02-exercise1.py
@@ -0,0 +1,8 @@
+# print exactly this to output:
+# Day 1 - Python Print Function
+# This function is declared like this:
+# print('what to print')
+
+print("Day 1 - Python Print Function")
+print("This function is declared like this:")
+print("print('what to print')")

3f6798a7bfa4878c8177d3f4c560e1e768afe43e
Tim Ngeno <h34r54y5@gmail.com>
2023-05-31 12:33:15 +0300

    use conditional if...else to evaluate statements

---
+++ day03/1-if_else.py
@@ -0,0 +1,10 @@
+# CONTROL FLOW WITH IF/ELSE AND CONDITIONAL OPERATORS
+#
+
+print("Welcome to the rollercoaster!")
+height = int(input("What is your height in cm? "))
+
+if height >= 120:
+    print("Yay..you can ride in the rollercoaster!")
+else:
+    print("Sorry, you need to grow taller to ride in the rollercoaster")

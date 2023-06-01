d477ff0049429d4708577f2e8c375b6f47332a51
Tim Ngeno <h34r54y5@gmail.com>
2023-06-01 13:38:20 +0300

    use logical operators to evaluate statements

---
+++ day03/8-logical_operators.py
@@ -0,0 +1,13 @@
+# LOGICAL OPERATORS
+
+# AND  - ALL CONDITIONS HAVE TO BE TRUE FOR THE CONDITION TO BE TRUE
+a = 12
+print(a > 10 and a < 13)
+print(a > 14 and a < 15)
+
+# OR - AT LEAST ONE OF THE CONDITIONS HAS TO BE TRUE TO EVALUATE AS TRUE
+print(a > 13 or a < 10)
+print(a < 14 or a > 15)
+
+# NOT - REVERSES THE CONDITION
+print(not a > 12)

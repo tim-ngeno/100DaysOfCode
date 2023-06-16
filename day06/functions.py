8ced21bba320befcc46bf374c431e8fcda0f6f10
Tim Ngeno <h34r54y5@gmail.com>
2023-06-16 20:31:25 +0300

    defining functions in python

---
+++ day06/functions.py
@@ -0,0 +1,7 @@
+# Write a function tp say hello and goodbye to the user
+def greeting(name):
+    print(f"Hello, {name}")
+    print(f"Goodbye, {name}")
+
+
+greeting(name=input("What is your name?: "))

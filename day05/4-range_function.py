3e98d40841696f7d206318f6e181720da60f61b4
Tim Ngeno <h34r54y5@gmail.com>
2023-06-16 19:48:44 +0300

    use the range function to return sum of 1-100

---
+++ day05/4-range_function.py
@@ -0,0 +1,8 @@
+# The range function
+# adding numbers between 1 and 100
+print("Using range function to calculate sum from 1 - 100")
+total = 0
+for number in range(1, 101):
+    total += number
+
+print(total)

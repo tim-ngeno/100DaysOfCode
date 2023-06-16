d778f7defbd8b7a84c7d15dd00f8dd60633bb0b8
Tim Ngeno <h34r54y5@gmail.com>
2023-06-16 18:20:22 +0300

    use the random module to generate random integers and floats

---
+++ day04/1-random_module.py
@@ -0,0 +1,10 @@
+import random
+
+random_integer = random.randint(1, 20)
+print(random_integer)
+
+random_float = random.random()
+print(random_float)
+
+another_random_float = random.random() * 5
+print(another_random_float)

59914c6ba27b2a75bfddf273360bd2f852d87c2a
Tim Ngeno <h34r54y5@gmail.com>
2023-06-16 18:49:59 +0300

    use lists to hold multiple variables

---
+++ day04/3-lists.py
@@ -0,0 +1,21 @@
+#  LISTS
+
+states_of_america = [
+    "Delaware", "Pennsylvania", "New Jersey", "Georgia", "Connecticut",
+    "Massachusetts", "Maryland", "South Carolina", "New Hampshire",
+    "Virginia", "New York", "North Carolina", "Rhode Island", "Vermont",
+    "Kentucky", "Tennessee", "Ohio", "Louisiana", "Indiana",
+    "Mississipi", "Illinois", "Alabama", "Maine", "Missouri",
+    "Arkansas", "Michigan", "Florida", "Texas", "Iowa", "Wisconsin",
+    "California", "Minnesota", "Oregon", "Kansas", " West Virginia",
+    "Nevada", "Nebraska", "Colorado", "North Dakota", "South Dakota",
+    "Montana", "Washington", "Idaho", "Wyoming", "Utah", "Oklahoma",
+    "New Mexico", "Arizona", "Alaska", "Hawaii"
+]
+print(len(states_of_america))
+
+# states_of_america.append("East Virginia")
+# print(states_of_america)
+
+states_of_america.extend(["North Carolina", "South Carolina"])
+print(states_of_america)

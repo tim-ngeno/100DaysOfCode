f71e95dd5b4b86cf30f44d3a5a64f9522693dbb9
Tim Ngeno <h34r54y5@gmail.com>
2023-07-21 00:01:48 +0300

    define constants in a program

---
+++ day12/1-constants.py
@@ -0,0 +1,8 @@
+"""
+Constant variables like mathematical formulas
+or URLS (variables that won't be modified in the program)
+are conventionally written in all UPPERCASE
+"""
+# Global constants
+PI = 3.14159
+URL = "https://www.google.com"

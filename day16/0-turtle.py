3163c8e452e01289dd4d649124b2f21da5d887f1
Tim Ngeno <h34r54y5@gmail.com>
2023-08-03 22:34:36 +0300

    use turtle package to implement OOP

---
+++ day16/0-turtle.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# Using turtle graphics: classes and methods
+from turtle import Turtle, Screen
+
+tim = Turtle()
+tim.shape("turtle")
+tim.color("coral")
+tim.forward(100)
+
+screen = Screen()
+
+screen.exitonclick()

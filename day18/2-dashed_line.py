6db7ab370b2ce1eedc9b78769755c8c35174bd9e
Tim Ngeno <h34r54y5@gmail.com>
2023-08-07 19:56:12 +0300

    draw a dashed line

---
+++ day18/2-dashed_line.py
@@ -0,0 +1,16 @@
+#!/usr/bin/envpython3
+"""Draw a dashed line"""
+
+from turtle import Turtle, Screen
+
+t = Turtle()
+
+for _ in range(15):
+    t.forward(10)
+    t.penup()
+    t.forward(10)
+    t.pendown()
+
+
+screen = Screen()
+screen.exitonclick()

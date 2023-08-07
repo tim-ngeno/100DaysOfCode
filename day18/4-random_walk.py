62e3ab043b00c49477365a67a5c3a71e4bea3190
Tim Ngeno <h34r54y5@gmail.com>
2023-08-07 19:58:22 +0300

    draw a random walk

---
+++ day18/4-random_walk.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+"""Create a Random Walk"""
+import random
+import turtle
+
+t = turtle.Turtle()
+turtle.colormode(255)
+
+
+def random_color():
+    """Generates a random RGB sequence color for out turtle"""
+    r = random.randint(0, 255)
+    g = random.randint(0, 255)
+    b = random.randint(0, 255)
+    return (r, g, b)
+
+
+pos = [90, 180, 270, 360]
+t.pensize(4)
+t.speed("fastest")
+
+for _ in range(500):
+    t.color(random_color())
+    t.forward(20)
+    t.setheading(random.choice(pos))
+
+screen = turtle.Screen()
+screen.exitonclick()

e9c13f8c33f5cc438870990d8588ca72ec7b6c4d
Tim Ngeno <timngeno424@gmail.com>
2024-08-26 15:03:30 +0300

    Get a notification if the ISS is overhead

---
+++ day33/ISS-Overhead/main.py
@@ -0,0 +1,70 @@
+import requests
+import smtplib
+import os
+import time
+from datetime import datetime
+
+
+MY_LAT = 51.507351  # Your latitude
+MY_LONG = -0.127758  # Your longitude
+
+MY_EMAIL = os.getenv("MY_EMAIL")
+MY_PASSWORD = os.getenv("MY_PASSWORD")
+
+
+def is_iss_overhead():
+    """Checks if the ISS is close to my current position"""
+    response = requests.get(url="http://api.open-notify.org/iss-now.json")
+    response.raise_for_status()
+    data = response.json()
+
+    iss_latitude = float(data["iss_position"]["latitude"])
+    iss_longitude = float(data["iss_position"]["longitude"])
+
+    # Your position is within +5 or -5 degrees of the ISS position.
+    if (
+        MY_LAT - 5 <= iss_latitude <= MY_LAT + 5
+        and MY_LONG - 5 <= iss_longitude <= MY_LONG + 5
+    ):
+        return True
+
+
+def is_night_time():
+    """Checks if it is night time while the ISS is overhead."""
+
+    parameters = {
+        "lat": MY_LAT,
+        "lng": MY_LONG,
+        "formatted": 0,
+    }
+
+    response = requests.get("https://api.sunrise-sunset.org/json", params=parameters)
+    response.raise_for_status()
+    data = response.json()
+    sunrise = int(data["results"]["sunrise"].split("T")[1].split(":")[0])
+    sunset = int(data["results"]["sunset"].split("T")[1].split(":")[0])
+
+    time_now = datetime.now().hour
+
+    if time_now >= sunset or time_now <= sunrise:
+        return True
+
+
+# If the ISS is close to my current position
+# and it is currently dark
+# Then send me an email to tell me to look up.
+# BONUS: run the code every 60 seconds.
+
+
+while True():
+    time.sleep(60)
+
+    if is_iss_overhead() and is_night_time():
+        connection = smtplib.SMTP("smtp.gmail.com")
+        connection.starttls()
+        connection.login(MY_EMAIL, MY_PASSWORD)
+        connection.sendmail(
+            from_addr=MY_EMAIL,
+            to_addr=MY_EMAIL,
+            msg="subject: Look UP!!🌟\n\nThe ISS is above you in the sky.",
+        )
